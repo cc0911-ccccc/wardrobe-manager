51c1e7d3e0f3644f5f8f2e99d05538ef2fcdf42f
cc0911-ccccc <k01049450977@gmail.com>
2026-09-07 10:22:41 +0800

    修复手动衣橱管理功能（添加/编辑/删除/筛选/记录）并配置云端 APK 编译

--- lib/widgets/clothing_card.dart
+++ lib/widgets/clothing_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/models.dart';
+import '../providers/wardrobe_provider.dart';
 
 class ClothingCard extends StatelessWidget {
   final Clothing clothing;
